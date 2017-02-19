d57efcea95c16a8bcb3273399a12a9f1eb61f032
Paco Ríos <pacoriosgalan@gmail.com>
2017-02-19 03:04:50 +0100

    Incluir el correo al proveedor al generar el pedido. Incompleto

--- public_html/codigo/nuevoPedido.php
+++ public_html/codigo/nuevoPedido.php
@@ -1,6 +1,12 @@
 <?php
 
+/*
+1. Guardar/Actualizar pedido en la BBDD
+2. Enviar correo al proveedor
+*/
+
 header('Access-Control-Allow-Origin: *');
+require_once('../Librerias/class.phpmailer.php');
 //PARA PRODUCCIÓN
 include ('../nietoBack/inc/conexion.php');
 
@@ -79,13 +85,154 @@ if (!$id_ppto){
 $query= "UPDATE pruebas_pedidos SET generado = 'S' WHERE id_ppto = $id_ppto";
 mysqli_query($link, $query);
 
+//Enviar correo
+$mail = new PHPMailer(); 
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "smtp.nietogranturismo.com"; 
+$mail->Username = "paco@nietogranturismo.com";
+$mail->Password = "Biturbo5"; 
+$mail->Port = 587; 
+
+$mail->From = "paco@nietogranturismo.com";
+$mail->FromName = "Paco NietoGranTurismo";
+
+//el valor por defecto 10 de Timeout es un poco escaso dado que voy a usar una cuenta gratuita, por tanto lo pongo a 30  
+$mail->Timeout=30;
+
+//Indicamos cual es la dirección de destino del correo
+$mail->AddAddress('pacoriosgalan@gmail.com');
+
+$articulos = '';
+foreach ($descripcion as $clave=>$valor)
+{
+  if (!$descripcion[$clave]==''){
+    if($pvp[$clave]==''){
+      $articulos .= '<tr><td class="ref">'.$ref[$clave].'</td>
+      <td class="desc" style="width:50%">'.$descripcion[$clave].'</td>
+      <td class="pvp">'.$precio[$clave].'</td>
+      <td class="uds">'.$uds[$clave].'</td>
+      <td class="importe">'.$total[$clave].'</td>
+      </tr>';
+    }
+    else{
+      $articulos .= '<tr><td class="ref">'.$ref[$clave].'</td>
+      <td class="desc" style="width:50%">'.$descripcion[$clave].'</td>
+      <td class="pvp">'.$pvp[$clave].'</td>
+      <td class="uds">'.$uds[$clave].'</td>
+      <td class="importe">'.$total[$clave].'</td>
+      </tr>';
+    }
+  }
+}
+
+$html='
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Presupuesto NIETO GRAN TURISMO. TLF - 654 777 777 - comercial@nietogranturismo.com</title>
+    <link href="../css/presupuesto_style.css" rel="stylesheet" type="text/css"/>  
+  </head>
+  <body>
+  <div class="contenido">
+    <header class="clearfix">
+      <h1>Presupuesto</h1> 
+      <div id="project">
+        <div><span>CLIENTE</span>'.utf8_encode($cliente_newPpto).'</div>
+        <div><span>VEHÍCULO</span>'.utf8_encode($vehiculo_newPpto).'</div>
+        <div><span>FECHA </span>'.$fecha_newPpto.'</div>
+      </div>
+    </header>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th class="ref">REFERENCIA</th>
+            <th class="desc" style="width:50%">DESCRIPCIÓN</th>
+            <th>PVP</th>
+            <th>UDS</th>
+            <th>IMPORTE</th>
+          </tr>
+        </thead>
+        <tbody>'.$articulos.'
+          <tr>
+            <td colspan="4" class="grand total">SUBTOTAL</td>
+            <td class="grand">'.$subtotal.'€</td>
+          </tr>
+          <tr>
+            <td colspan="4">IVA</td>
+            <td>'.$iva_newPpto.'%</td>
+          </tr>
+          <tr>
+            <td colspan="4"><strong>TOTAL</strong></td>
+            <td><strong>'.$totalTotal.'€</strong></td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
+      <br />
+      <div id="notices">
+        <div>Comentarios:</div>
+        <div class="notice">'.utf8_encode($asunto_newPpto).'</div>
+      </div>
+    </main>
+    <footer>
+      NIETO GRAN TURISMO. TLF - 654 777 777 - comercial@nietogranturismo.com
+    </footer>
+  </div>
+  </body>
+</html>';
+
+//Asignamos asunto y cuerpo del mensaje
+//El cuerpo del mensaje lo ponemos en formato html, haciendo 
+//que se vea en negrita
+$mail->Subject = "Nieto GranTurismo. Order.";
+$mail->Body = $html;
+//$mail->Body += "<p>I want to order for a $COCHE .-</p>";
+
+//Definimos AltBody por si el destinatario del correo no admite email con formato html 
+$mail->AltBody = "Mensaje de prueba mandado con phpmailer en formato solo texto";
+
+//se envia el mensaje, si no ha habido problemas 
+//la variable $exito tendra el valor true
+$exito = $mail->Send();
+
+//Si el mensaje no ha podido ser enviado se realizaran 4 intentos mas con un intervalo de 5seg
+$intentos=1; 
+while ((!$exito) && ($intentos < 5)) {
+  sleep(5);
+  //echo $mail->ErrorInfo;
+  $exito = $mail->Send();
+  $intentos=$intentos+1;  
+}
+ 
+    
+if(!$exito)
+{
+  echo "Problemas enviando correo electrónico a ".$valor;
+  echo "<br/>".$mail->ErrorInfo; 
+  ?>
+  <script language="javascript">
+  console.log('Mensaje NO enviado');
+  </script> 
+  <?php 
+}
+else
+{
+  ?>
+  <script language="javascript">
+  console.log('Mensaje enviado');
+  </script> 
+<?php
+}
+
 ?>
 <script language="javascript">
     //LOCALHOST
     //window.location='/nieto/public_html/index.php';
 
     //PRODUCCIÓN
-    window.location='http://admin.nietogranturismo.com/';    
+    //window.location='http://admin.nietogranturismo.com/';    
 </script> 
 <?php
 
