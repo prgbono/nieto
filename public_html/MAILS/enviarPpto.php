e6c600c401ce56c9c768ec6510535f8e2d78812a
Paco Ríos <pacoriosgalan@gmail.com>
2017-03-22 07:42:02 +0100

    Guardar un presupuesto al pulsar sobre Enviar en el apartado Nuevo Presupuesto

--- public_html/MAILS/enviarPpto.php
+++ public_html/MAILS/enviarPpto.php
@@ -19,12 +19,83 @@ extract($_POST);
   }
 }*/
 
+/*GUARDAR PPTO (mismo código que código/nuevoPpto)*/
+$canarias_newPpto = isset($_POST['canarias_newPpto']) ? 1 : 0;
+$iva_newPpto = !($_POST['iva_newPpto'] == '') ? $_POST['iva_newPpto'] : 21;
+$totalTotal = str_replace(',','.',$totalTotal);
+$subtotal = str_replace(',','.',$subtotal);
+$fecha_newPpto = str_replace('-','/',$fecha_newPpto);
+
+
+if ($id_ppto){
+    $mensaje = 'MOFIDICACIÓN';
+    /*1.-MODIFICAR EN LA TABLA DE PRESUPUESTOS*/
+    $query= "UPDATE pruebas_presupuestos SET fecha = STR_TO_DATE('$fecha_newPpto', '%d/%m/%Y'), 
+        id_coche = '".$vehiculo_newPpto."', id_cliente = '".$cliente_newPpto."', asunto = '$asunto_newPpto', 
+        total = '$totalTotal', transporte = '$transporte_newPpto', canarias = '".$canarias_newPpto."', 
+        subtotal = '$subtotal', iva = '".$iva_newPpto."' WHERE id_ppto = '$id_ppto' ";
+    /*echo $query."\n";*/
+    mysqli_query($link, $query);
+    
+    /* 2.- ELIMINAR TODOS LOS REGISTROS ASOCIADOS a ese id_ppto EN LA TABLA DE DETALLES */
+    $query3= "DELETE FROM pruebas_detalle_presupuestos WHERE id_ppto = '$id_ppto' ";
+    /*echo $query3."\n";*/
+    mysqli_query($link, $query3);
+
+    /* 3.- REESCRIBIR la tabla de detalles (Acción común con la inserción)*/
+}
+else{
+    $mensaje = 'INSERCIÓN';
+    
+    /*1.-INSERTAR EN LA TABLA DE PRESUPUESTOS*/
+    //Para localhost el formato debe ser '%d/%m/%Y' instead of '%d-%m-%Y'
+    $query= "INSERT INTO pruebas_presupuestos (fecha, id_coche, id_cliente, asunto, total, transporte, canarias, subtotal, iva) VALUES (STR_TO_DATE('$fecha_newPpto', '%d/%m/%Y'), '".$vehiculo_newPpto."', '".$cliente_newPpto."', '$asunto_newPpto', '$totalTotal', '$transporte_newPpto', '".$canarias_newPpto."', '$subtotal', '".$iva_newPpto."')";
+    //echo $query."\n";
+    mysqli_query($link, $query);
+
+    //Obtengo el id_ppto recién insentado para usarlo en la tabla de detalles_presupuestos
+    $ppto =  mysqli_query($link, "SELECT MAX(id_ppto) as maxId_ppto FROM pruebas_presupuestos"); 
+    $maxPpto = mysqli_fetch_assoc($ppto);
+    $id_ppto = $maxPpto['maxId_ppto'];
+
+}
+
+//Con esto tengo en cada array exactamente lo que ha metido el usuario. Independientemente si ha metido alguna descripción y después no unidades. TENGO LOS ARRAYS SIN POSICIONES VACÍAS.
+$descripcion=array_filter($descripcion);
+$ref=array_filter($ref);
+$precio=array_filter($precio);
+$uds=array_filter($uds);
+$cambio=array_filter($cambio);
+$pvp=array_filter($pvp);
+$dto=array_filter($dto);
+$total=array_filter($total);
+
+$query2="INSERT INTO pruebas_detalle_presupuestos (id_ppto, descripcion, referencia, uds, precio, cambio, pvp, dto, total) VALUES ";   
+foreach ($descripcion as $clave=>$valor)
+{
+    $precio = str_replace(',','.',$precio);
+    $cambio = str_replace(',','.',$cambio);
+    $pvp = str_replace(',','.',$pvp);
+    $total = str_replace(',','.',$total);
+    $query2.= "(".$id_ppto.",'$valor', '".$ref[$clave]."', ".$uds[$clave].", '".$precio[$clave]."', '".$cambio[$clave]."', '".$pvp[$clave]."', '".$dto[$clave]."', '".$total[$clave]."'),";
+}
+$query2 = substr($query2, 0, -1);
+
+/*echo $query;
+echo $query2;*/
+
+mysqli_query($link, $query2); 
+/*FIN GUARDAR PPTO*/
+
+//Envío al cliente
 //Obtengo correo del cliente
-$id_cliente = !($_POST['id_cliente'] == '') ? $_POST['id_cliente'] : '';
+$cliente_seleccionado = isset($_POST['cliente_newPpto']) ? $_POST['cliente_newPpto'] : '';
+$id_cliente = !($_POST['id_cliente'] == '') ? $_POST['id_cliente'] : $cliente_seleccionado;
 $correo = "SELECT email FROM pruebas_clientes WHERE id_cliente = '$id_cliente'";
 $result = mysqli_query($link, $correo);
 $correo= mysqli_fetch_assoc($result);
 $correo=$correo['email']; 
+
 
 /*Enviar correo
 Obtener datos necesarios para formar el correo:
@@ -191,6 +262,7 @@ if(!$exito)
   ?>
   <script language="javascript">
   console.log('Mensaje NO enviado');
+  window.location='http://admin.nietogranturismo.com/';
   </script> 
   <?php 
 }
